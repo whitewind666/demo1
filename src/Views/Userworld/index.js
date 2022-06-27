a496c3569d288f4e65984b7700bfd37a46ba8ee6
whitewind666 <963567023@qq.com>
2022-06-27 16:43:12 +0800

    6.27

--- src/Views/Userworld/index.js
+++ src/Views/Userworld/index.js
@@ -4,39 +4,41 @@ import {Button, Card, Input, List,} from 'antd';
 
 import './style.css'
 
-const { TextArea } = Input;
+const {TextArea} = Input;
+
 class Userworld extends Component {
-    constructor(props){
+    state = {
+        header: '',
+        info: '',
+        telephone: '',
+        userweb: '',
+        bbll: ''
+    }
+
+    constructor(props) {
         super(props);
         this.headerList = []
-            for (let i =0; i<20 ;i++){
-               this.headerList.push({
-                   text:'头像'+(i+1),
-                   icon:require(`../../assets/images/headers/头像${i+1}.png`)
-               })
+        for (let i = 0; i < 20; i++) {
+            this.headerList.push({
+                text: '头像' + (i + 1),
+                icon: require(`../../assets/images/headers/头像${i + 1}.png`)
+            })
         }
     }
 
 
-    state={
-        header:'',
-        info:'',
-        telephone:'',
-        userweb:'',
-        bbll:''
-    }
-    handelChange =(name,val)=>{
+    handleChange = (name, val) => {
         this.setState({
-            [name] : val.target.value
+            [name]: val.target.value
         })
     }
 
-    save=()=>{
+    save = () => {
         console.log(this.state)
     }
 
     render() {
-        const data =this.headerList;
+        const data = this.headerList;
         return (
             <div className="user_content">
                 <div className="user_body">
@@ -52,15 +54,18 @@ class Userworld extends Component {
                         header={<div>选择头像</div>}
                         grid={{
                             gutter: 1,
-                            columns:3,
+                            columns: 3,
                         }}
                         dataSource={data}
                         renderItem={(item) => (
                             <List.Item>
                                 <Card title={item.title}>
-                                   <div>
+                                    <div>
+                                        {this.state.headerList.map((headerPic, index) => (
+                                            <div key={index}> {headerPic.text}  {headerPic.icon}</div>
+                                        ))}
 
-                                   </div>
+                                    </div>
                                 </Card>
                             </List.Item>
                         )}
@@ -68,31 +73,43 @@ class Userworld extends Component {
                     </List>
 
                     {/* input框 */}
-                    <div className="user_input"  align="middle" style={{minHeight:'100vh'}}>
-                        <Input.Group compact
-                                     onChange={val=>{this.handleChange('info',val)}}
-                        >
-                            <Input style={{ width: '10%' }} defaultValue="昵称" />
-                            <Input style={{ width: '30%' }}  placeholder="请输入昵称" />
-                        </Input.Group>
-                        <br/>
-                        <Input.Group compact
-                                     onChange={val=>{this.handleChange('telephone',val)}}
-                        >
-                            <Input style={{ width: '10%' }} defaultValue="电话" />
-                            <Input style={{ width: '30%' }}  placeholder="请输入电话" />
-                        </Input.Group>
-                        <br/>
-                        <Input.Group compact
-                                     onChange={val=>{this.handleChange('userweb',val)}}
-                        >
-                            <Input style={{ width: '10%' }} defaultValue="个人网站" />
-                            <Input style={{ width: '30%' }}  placeholder="请输入邮箱地址" />
+                    <div className="user_input" align="middle" style={{minHeight: '100vh'}}>
+                        <Input.Group compact>
+                            <Input style={{width: '10%'}} defaultValue="昵称"/>
+                            <Input style={{width: '30%'}}
+                                   placeholder="请输入昵称"
+                                   onChange={val => {
+                                       this.handleChange('info', val)
+                                   }}
+                            />
                         </Input.Group>
                         <br/>
                         <Input.Group compact>
-                            <Input style={{ width: '10%' }} defaultValue="0571" />
-                            <Input style={{ width: '30%' }} defaultValue="26888888" />
+                            <Input style={{width: '10%'}} defaultValue="电话"/>
+                            <Input style={{width: '30%'}}
+                                   type="number"
+                                   placeholder="请输入电话"
+                                   step="0.01" min="10000000000" max="19999999999"
+                                   onChange={val => {
+                                       this.handleChange('telephone', val)
+                                   }}
+                            />
+                        </Input.Group>
+                        <br/>
+                        <Input.Group compact>
+                            <Input style={{width: '10%'}} defaultValue="个人网站"/>
+                            <Input style={{width: '30%'}}
+                                   type="email"
+                                   placeholder="请输入邮箱地址"
+                                   onChange={val => {
+                                       this.handleChange('userweb', val)
+                                   }}
+                            />
+                        </Input.Group>
+                        <br/>
+                        <Input.Group compact>
+                            <Input style={{width: '10%'}} defaultValue="0571"/>
+                            <Input style={{width: '30%'}} defaultValue="26888888"/>
                         </Input.Group>
                         <br/>
                         <div className="introduction">
@@ -100,7 +117,9 @@ class Userworld extends Component {
                             <TextArea rows={4} maxLength={200}
                                       placeholder="简介"
                                       style={{resize: 'none'}}
-                                      onChange={val=>{this.handleChange('bbll',val)}}
+                                      onChange={val => {
+                                          this.handleChange('bbll', val)
+                                      }}
                             />
                         </div>
                         <br/>
@@ -115,6 +134,6 @@ class Userworld extends Component {
 }
 
 export default connect(
-    state=>({user: state.user}),
+    state => ({user: state.user}),
     {}
 )(Userworld)
